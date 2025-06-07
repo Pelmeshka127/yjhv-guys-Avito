413b66d6f7065c6b3ac302bb0d6c86a5ee28623e
Kirill Pavlovskiy <kirya0703@gmail.com>
2025-06-07 21:17:13 +0300

    4: run with docker

--- src/main.py
+++ src/main.py
@@ -96,4 +96,4 @@ def get():
     return jsonify({'hello': 'hello'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=8081)
+    app.run(debug=True, host="0.0.0.0", port=8082)
